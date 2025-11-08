aff36a6a8c0986e61955d9907d40ac2c6429fec4
Kristóf Császár <csaszar.kristof.felhasznalo@gmail.com>
2025-11-08 15:48:05 +0100

    TDD exercises

--- tdd/app.py
+++ tdd/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,3 +7,12 @@ todos = [
     {"id": 1, "task": "Learn TDD", "done": False},
     {"id": 2, "task": "Build a Flask API", "done": True},
 ]
+
+@app.route('/')
+def index():
+    return "Welcome"
+
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    if request.method == "GET":
+        return jsonify(todos)
